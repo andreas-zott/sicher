07f32e09708cac73d383c27f65000f8ee1b114c8
andreas-zott <quadzott@gmail.com>
2025-05-22 18:22:39 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-// service-worker.js
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('checkliste-v1').then(cache =>
-      cache.addAll([
+    caches.open('checkliste-cache-v1').then(cache => {
+      return cache.addAll([
         '/',
         '/index.html',
-        '/style.css',
-        '/script.js'
-        // + alle weiteren Assets
-      ])
-    )
+        '/manifest.json',
+        '/icon.png'
+      ]);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
