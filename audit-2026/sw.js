9b934d1ef1d4e37da4917ea23c6372c261bb7dba
andreas-zott <quadzott@gmail.com>
2026-06-08 12:11:15 +0200

    Add files via upload

---
+++ audit-2026/sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('checkliste-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon.png',
+        '/favicon.ico',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
