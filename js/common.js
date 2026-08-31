0ba92fce0a570276513c95bd2e37ca86fc761a15
andreas-zott <quadzott@gmail.com>
2026-08-31 06:27:39 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -6,7 +6,7 @@
 // Service-Workern (sw.js / service-worker.js), die sich gegenseitig
 // ueberschrieben haben. Jetzt: eine Quelle, ein Service Worker.
 
-const APP_VERSION = '1.0.0';
+const APP_VERSION = '1.0.1';
 
 function initThemeToggle() {
     const toggle = document.getElementById('themeToggle');
